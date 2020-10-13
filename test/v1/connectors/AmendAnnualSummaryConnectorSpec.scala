b71fbf03bf7cd55975262d4d9075c9cd7beaf914
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2020-10-13 14:24:26 +0100

    conector and service

---
+++ test/v1/connectors/AmendAnnualSummaryConnectorSpec.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import mocks.MockAppConfig
+import uk.gov.hmrc.domain.Nino
+import v1.mocks.MockHttpClient
+import v1.models.domain.DesTaxYear
+import v1.models.domain.exemptionCode.MtdExemptionCode
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.amendAnnualSummary.{Adjustments, Allowances, AmendAnnualSummaryBody, AmendAnnualSummaryRequest, Class4NicInfo, NonFinancials}
+
+import scala.concurrent.Future
+
+class AmendAnnualSummaryConnectorSpec extends ConnectorSpec {
+
+  val nino: String = "AA123456A"
+  val taxYear: String = "2018-19"
+  val businessId: String = "XAIS12345678910"
+
+  val request: AmendAnnualSummaryRequest = AmendAnnualSummaryRequest(
+    nino = Nino(nino),
+    businessId = businessId,
+    taxYear = taxYear,
+    body = AmendAnnualSummaryBody(
+        Some(Adjustments(Some(1.11), Some(2.22), Some(3.33), Some(4.44), Some(5.55), Some(6.66), Some(7.77), Some(8.88), Some(9.99), Some(10.10))),
+        Some(Allowances(Some(1.11), Some(2.22), Some(3.33), Some(4.44), Some(5.55), Some(6.66), Some(7.77), Some(8.88), Some(9.99))),
+        Some(NonFinancials(Some(Class4NicInfo(isExempt = true, Some(MtdExemptionCode.`001 - Non Resident`)))))
+    )
+  )
+
+  class Test extends MockHttpClient with MockAppConfig {
+
+    val connector: AmendAnnualSummaryConnector = new AmendAnnualSummaryConnector(
+      http = mockHttpClient,
+      appConfig = mockAppConfig
+    )
+
+    val desRequestHeaders: Seq[(String, String)] = Seq(
+      "Environment" -> "des-environment",
+      "Authorization" -> s"Bearer des-token"
+    )
+
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  "AmendAnnualSummaryConnector" when {
+    "amendAnnualSummary" must {
+      "return a 204 status for a success scenario" in new Test {
+        val outcome = Right(ResponseWrapper(correlationId, ()))
+
+        MockedHttpClient
+          .put(
+            url = s"$baseUrl/income-tax/nino/$nino/self-employments/$businessId/annual-summaries/${DesTaxYear.fromMtd(taxYear)}",
+            body = request.body,
+            requiredHeaders = desRequestHeaders: _*
+          ).returns(Future.successful(outcome))
+
+        await(connector.amendAnnualSummary(request)) shouldBe outcome
+      }
+    }
+  }
+}
---
+++ test/v1/services/AmendAnnualSummaryServiceSpec.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import uk.gov.hmrc.domain.Nino
+import v1.controllers.EndpointLogContext
+import v1.mocks.connectors.MockAmendAnnualSummaryConnector
+import v1.models.domain.DesTaxYear
+import v1.models.domain.exemptionCode.MtdExemptionCode
+import v1.models.errors.{DesErrorCode, DesErrors, DownstreamError, ErrorWrapper, MtdError, NinoFormatError, NotFoundError, TaxYearFormatError}
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.amendAnnualSummary.{Adjustments, Allowances, AmendAnnualSummaryBody, AmendAnnualSummaryRequest, Class4NicInfo, NonFinancials}
+
+import scala.concurrent.Future
+
+class AmendAnnualSummaryServiceSpec extends ServiceSpec {
+
+  private val nino = "AA123456A"
+  val businessId = ""
+  private val taxYear = "2017-18"
+  private val correlationId = "X-123"
+
+  private val requestBody =  AmendAnnualSummaryBody(
+    Some(Adjustments(Some(1.11), Some(2.22), Some(3.33), Some(4.44), Some(5.55), Some(6.66), Some(7.77), Some(8.88), Some(9.99), Some(10.10))),
+    Some(Allowances(Some(1.11), Some(2.22), Some(3.33), Some(4.44), Some(5.55), Some(6.66), Some(7.77), Some(8.88), Some(9.99))),
+    Some(NonFinancials(Some(Class4NicInfo(isExempt = true, Some(MtdExemptionCode.`001 - Non Resident`)))))
+  )
+
+  private val requestData = AmendAnnualSummaryRequest(
+    nino = Nino(nino),
+    businessId = businessId,
+    taxYear = taxYear,
+    body = requestBody
+  )
+
+  trait Test extends MockAmendAnnualSummaryConnector {
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service = new AmendAnnualSummaryService(
+      connector = mockAmendAnnualSummaryConnector
+    )
+  }
+
+  "AmendAnnualSummaryService" when {
+    "amendAnnualSummary" must {
+      "return correct result for a success" in new Test {
+        val outcome = Right(ResponseWrapper(correlationId, ()))
+
+        MockAmendAnnualSummaryConnector.amendAnnualSummary(requestData)
+          .returns(Future.successful(outcome))
+
+        await(service.amendAnnualSummary(requestData)) shouldBe outcome
+      }
+    }
+
+    "map errors according to spec" when {
+
+      def serviceError(desErrorCode: String, error: MtdError): Unit =
+        s"a $desErrorCode error is returned from the service" in new Test {
+
+          MockAmendAnnualSummaryConnector.amendAnnualSummary(requestData)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DesErrors.single(DesErrorCode(desErrorCode))))))
+
+          await(service.amendAnnualSummary(requestData)) shouldBe Left(ErrorWrapper(Some(correlationId), error))
+        }
+
+      val input = Seq(
+        ("INVALID_NINO", NinoFormatError),
+        ("INVALID_TAX_YEAR", TaxYearFormatError),
+        ("INVALID_INCOME_SOURCE", ???),
+        ("NOT_FOUND_INCOME_SOURCE", NotFoundError),
+        ("SERVER_ERROR", DownstreamError),
+        ("SERVICE_UNAVAILABLE", DownstreamError)
+      )
+
+      input.foreach(args => (serviceError _).tupled(args))
+    }
+  }
+}
