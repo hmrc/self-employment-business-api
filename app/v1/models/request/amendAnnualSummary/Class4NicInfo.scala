cb32e11d2b62d426672824b8281d28aa52634edb
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2020-10-14 10:16:59 +0100

    change file names

--- app/v1/models/request/amendAnnualSummary/Class4NicInfo.scala
+++ app/v1/models/request/amendAnnualSummary/Class4NicInfo.scala
@@ -18,39 +18,17 @@ package v1.models.request.amendAnnualSummary
 
 import play.api.libs.functional.syntax._
 import play.api.libs.json.{JsPath, Json, OWrites, Reads}
-<<<<<<< HEAD
-<<<<<<< HEAD
 import v1.models.domain.ex.{DesEx, MtdEx}
 
 case class Class4NicInfo(isExempt: Boolean, exemptionCode: Option[MtdEx])
-=======
-import v1.models.domain.exemptionCode.MtdExemptionCode
-=======
-import v1.models.domain.exemptionCode.{DesExemptionCode, MtdExemptionCode}
->>>>>>> 68f75cf... finish models and testing
-
-case class Class4NicInfo(isExempt: Boolean, exemptionCode: Option[MtdExemptionCode])
->>>>>>> a67f837... Amend Annual summary models
 
 object Class4NicInfo {
 
   implicit val reads: Reads[Class4NicInfo] = Json.reads[Class4NicInfo]
   implicit val writes: OWrites[Class4NicInfo] = (
     (JsPath \ "exemptFromPayingClass4Nics").write[Boolean] and
-<<<<<<< HEAD
-<<<<<<< HEAD
       (JsPath \ "class4NicsExemptionReason").writeNullable[DesEx]
     ) (unlift(Class4NicInfo.unapply(_: Class4NicInfo).map {
     case (bool, exemptionCode) => (bool, exemptionCode.map(_.toDes))
   }))
-=======
-      (JsPath \ "class4NicsExemptionReason").writeNullable[MtdExemptionCode]
-    ) (unlift(Class4NicInfo.unapply))
->>>>>>> a67f837... Amend Annual summary models
-=======
-      (JsPath \ "class4NicsExemptionReason").writeNullable[DesExemptionCode]
-    ) (unlift(Class4NicInfo.unapply(_: Class4NicInfo).map {
-    case (bool, exemptionCode) => (bool, exemptionCode.map(_.toDes))
-  }))
->>>>>>> 68f75cf... finish models and testing
 }
