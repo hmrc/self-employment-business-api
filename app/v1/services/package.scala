6732cb4d1cc619182b4efc1a76a1535b5b6fb12e
HarryBaxter <52996647+HarryBaxter@users.noreply.github.com>
2020-10-15 13:09:45 +0100

    MTDSA-7313 Connector and Service for Retrieve SE Annual Summary (#18)
    * MTDSA-7313 Connector and Service for Retrieve SE Annual Summary
    
    * MTDSA-7313 Connector and Service for Retrieve SE Annual Summary
    
    * Updated Url and Errors
    
    * MTDSA-7313 Uri amendment

--- app/v1/services/package.scala
+++ app/v1/services/package.scala
@@ -18,11 +18,14 @@ package v1
 
 import v1.models.errors.ErrorWrapper
 import v1.models.outcomes.ResponseWrapper
+import v1.models.response.retrieveSEAnnual.RetrieveSelfEmploymentAnnualSummaryResponseBody
 
 package object services {
 
   private type ServiceOutcome[A] = Either[ErrorWrapper, ResponseWrapper[A]]
 
   type DeleteSelfEmploymentAnnualSummaryServiceOutcome = ServiceOutcome[Unit]
+
+  type RetrieveSelfEmploymentAnnualSummaryServiceOutcome = ServiceOutcome[RetrieveSelfEmploymentAnnualSummaryResponseBody]
 
 }
