8026fe3572f36170ff91f134ff130779350305c7
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-10-28 11:18:27 +0000

    Models for listPeriodicUpdate

---
+++ app/v1/models/response/listSEPeriodic/ListSelfEmploymentPeriodicResponse.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.response.listSEPeriodic
+
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+
+case class ListSelfEmploymentPeriodicResponse(selfEmploymentPeriods: Seq[Option[PeriodDetails]])
+
+object ListSelfEmploymentPeriodicResponse {
+  implicit val reads: Reads[ListSelfEmploymentPeriodicResponse] =
+    (JsPath \ "periods").read[Seq[Option[PeriodDetails]]]
+
+  (ListSelfEmploymentPeriodicResponse.apply _)
+
+  implicit val writes: OWrites[ListSelfEmploymentPeriodicResponse] = Json.writes[ListSelfEmploymentPeriodicResponse]
+}
---
+++ test/v1/models/response/listSEPeriodic/ListSelfEmploymentPeriodicResponseSpec.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.response.listSEPeriodic
+
+import support.UnitSpec
+
+class ListSelfEmploymentPeriodicResponseSpec extends UnitSpec {
+
+  val beforeGenerateModel = ListSelfEmploymentPeriodicResponse(
+    Seq(PeriodDetailsObject(
+      Some(PeriodDetails(
+        "",
+        "2020-01-01",
+        "2020-01-01"
+      )))))
+
+  val afterGenerateModel = ListSelfEmploymentPeriodicResponse(
+    Seq(PeriodDetailsObject(
+      Some(PeriodDetails(
+        "2020-01-01_2020-01-01",
+        "2020-01-01",
+        "2020-01-01"
+      )))))
+
+  val beforeGenerateModelMultiple = ListSelfEmploymentPeriodicResponse(
+    Seq(PeriodDetailsObject(
+      Some(PeriodDetails(
+        "",
+        "2020-01-01",
+        "2020-01-01"
+      )))))
+
+  val afterGenerateModelMultiple = ListSelfEmploymentPeriodicResponse(
+    Seq(PeriodDetailsObject(
+      Some(PeriodDetails(
+        "2020-01-01_2020-01-01",
+        "2020-01-01",
+        "2020-01-01"
+      )))))
+
+  val json = Json.parse(
+    """
+      |{
+      |   "periodDetails": {
+      |       "periodId": "2020-01-01_2020-01-01",
+      |       "from": "2020-01-01",
+      |       "to": "2020-01-01"
+      |   }
+      |}
+      |""".stripMargin
+  )
+
+  val desJson = Json.parse(
+    """
+      |{
+      |   "periodDetails": {
+      |       "transactionReference": "111111111111",
+      |       "from": "2020-01-01",
+      |       "to": "2020-01-01"
+      |   }
+      |}
+      |""".stripMargin
+  )
+
+  "reads" should {
+
+    "read from a json" when {
+
+      "a valid request is made" in  {
+        desJson.as[PeriodDetailsObject] shouldBe beforeGenerateModel
+      }
+    }
+  }
+  "writes" should {
+
+    "write to a model" when {
+
+      "a valid request is made" in {
+        Json.toJson(afterGenerateModel) shouldBe json
+      }
+    }
+  }
+
