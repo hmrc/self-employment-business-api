642a8d225609564522698887eb30a854110b6c2e
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2020-10-29 09:15:27 +0000

    [MTDSA-7421] Amend SE Periodic Update Models (#38)
    * MTDSA-7421 Models initial commit
    
    * MTDSA-7421 read tests
    
    * MTDSA-7421 writes tests
    
    * MTDSA-7421 expenses and incomes specs
    
    * removed unused import
    
    Co-authored-by: DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>

---
+++ app/v1/models/request/amendSEPeriodic/AmendPeriodicBody.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.request.amendSEPeriodic
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+
+case class AmendPeriodicBody(incomes: Option[Incomes], consolidatedExpenses: Option[ConsolidatedExpenses], expenses: Option[Expenses])
+
+object AmendPeriodicBody {
+
+  implicit val reads: Reads[AmendPeriodicBody] = Json.reads[AmendPeriodicBody]
+
+  implicit val writes: OWrites[AmendPeriodicBody] = (
+      (JsPath \ "incomes").writeNullable[Incomes] and
+      (JsPath \ "deductions" \ "simplifiedExpenses").writeNullable[BigDecimal] and
+      (JsPath \ "deductions").writeNullable[Expenses]
+    )(unlift(AmendPeriodicBody.unapply(_: AmendPeriodicBody).map {
+    case (incomesO, consolidatedExpensesO, expensesO) =>
+      (incomesO, consolidatedExpensesO.map(_.consolidatedExpenses), expensesO)
+  }))
+}
