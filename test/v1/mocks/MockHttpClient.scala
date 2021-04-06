ff8b4ce863adbb1ae3c2106e5d5c7e0df7e7da8b
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2021-04-06 10:42:13 +0100

    [MTDSA-8359] - Dependency Upgrade  (#67)
    * updated dependencies
    
    * updated domain dependency
    
    * additional dependency upgrades
    
    * changed scalaversion
    
    * updated bootstrap-backen-play-27
    
    * continued dependency upgrades
    
    * further dependency upgrades
    
    * warnings resolved

--- test/v1/mocks/MockHttpClient.scala
+++ test/v1/mocks/MockHttpClient.scala
@@ -31,9 +31,9 @@ trait MockHttpClient extends MockFactory {
 
     def get[T](url: String, requiredHeaders: (String, String)*): CallHandler[Future[T]] = {
       (mockHttpClient
-        .GET(_: String)(_: HttpReads[T], _: HeaderCarrier, _: ExecutionContext))
-        .expects(where { (actualUrl, _, hc, _) =>
-          url == actualUrl && requiredHeaders.forall(h => hc.headers.contains(h))
+        .GET(_: String, _: Seq[(String, String)], _: Seq[(String, String)])(_: HttpReads[T], _: HeaderCarrier, _: ExecutionContext))
+        .expects(where { (actualUrl, _, _, _, hc, _) =>
+          url == actualUrl && requiredHeaders.forall((hc.extraHeaders ++ hc.headers(requiredHeaders.map(_._1))).contains)
         })
     }
 
@@ -41,7 +41,7 @@ trait MockHttpClient extends MockFactory {
       (mockHttpClient
         .POST[I, T](_: String, _: I, _: Seq[(String, String)])(_: Writes[I], _: HttpReads[T], _: HeaderCarrier, _: ExecutionContext))
         .expects(where { (actualUrl, actualBody, _, _, _, hc, _) =>
-          url == actualUrl && body == actualBody && requiredHeaders.forall(h => hc.headers.contains(h))
+          url == actualUrl && body == actualBody && requiredHeaders.forall((hc.extraHeaders ++ hc.headers(requiredHeaders.map(_._1))).contains)
         })
     }
 
@@ -49,7 +49,7 @@ trait MockHttpClient extends MockFactory {
       (mockHttpClient
         .DELETE(_: String, _: Seq[(String, String)])(_: HttpReads[T], _: HeaderCarrier, _: ExecutionContext))
         .expects(where { (actualUrl, _,  _, hc, _) =>
-          url == actualUrl && requiredHeaders.forall(h => hc.headers.contains(h))
+          url == actualUrl && requiredHeaders.forall((hc.extraHeaders ++ hc.headers(requiredHeaders.map(_._1))).contains)
         })
     }
 
@@ -57,8 +57,9 @@ trait MockHttpClient extends MockFactory {
       (mockHttpClient
         .PUT[I, T](_: String, _: I, _: Seq[(String, String)])(_: Writes[I], _: HttpReads[T], _: HeaderCarrier, _: ExecutionContext))
         .expects(where { (actualUrl, actualBody, _, _, _, hc, _) =>
-          url == actualUrl && body == actualBody && requiredHeaders.forall(h => hc.headers.contains(h))
+          url == actualUrl && body == actualBody && requiredHeaders.forall((hc.extraHeaders ++ hc.headers(requiredHeaders.map(_._1))).contains)
         })
     }
   }
+
 }
