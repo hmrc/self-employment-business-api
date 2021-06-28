bd5beb0b45f295405fc149d38429fdf8ca510b5d
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2021-06-28 11:42:36 +0100

    [MTDSA-9061] Updated format In AppConfig and changed import logger to logging

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -23,40 +23,40 @@ import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 trait AppConfig {
 
-  def desBaseUrl: String
-
+  //MTD ID LookupConfig
   def mtdIdBaseUrl: String
 
+  //Des config
+  def desBaseUrl: String
   def desEnv: String
-
   def desToken: String
+  def desEnvironmentHeaders: Option[Seq[String]]
 
+  //API Config
   def apiGatewayContext: String
-
   def apiStatus(version: String): String
-
   def featureSwitch: Option[Configuration]
-
   def endpointsEnabled(version: String): Boolean
-
   def confidenceLevelConfig: ConfidenceLevelConfig
 }
 
 @Singleton
 class AppConfigImpl @Inject()(config: ServicesConfig, configuration: Configuration) extends AppConfig {
 
+  //MTD ID Lookup Config
   val mtdIdBaseUrl: String = config.baseUrl("mtd-id-lookup")
+
+  //DES Config
   val desBaseUrl: String = config.baseUrl("des")
   val desEnv: String = config.getString("microservice.services.des.env")
   val desToken: String = config.getString("microservice.services.des.token")
+  val desEnvironmentHeaders: Option[Seq[String]] = configuration.getOptional[Seq[String]]("microservice.services.des.environmentHeaders")
+
+  //API Config
   val apiGatewayContext: String = config.getString("api.gateway.context")
-
   def apiStatus(version: String): String = config.getString(s"api.$version.status")
-
   def featureSwitch: Option[Configuration] = configuration.getOptional[Configuration](s"feature-switch")
-
   def endpointsEnabled(version: String): Boolean = config.getBoolean(s"api.$version.endpoints.enabled")
-
   val confidenceLevelConfig: ConfidenceLevelConfig = configuration.get[ConfidenceLevelConfig](s"api.confidence-level-check")
 }
 
@@ -65,8 +65,8 @@ trait FixedConfig {
   val minimumTaxYear = 2018
 }
 
-
 case class ConfidenceLevelConfig(definitionEnabled: Boolean, authValidationEnabled: Boolean)
+
 object ConfidenceLevelConfig {
   implicit val configLoader: ConfigLoader[ConfidenceLevelConfig] = (rootConfig: Config, path: String) => {
     val config = rootConfig.getConfig(path)
