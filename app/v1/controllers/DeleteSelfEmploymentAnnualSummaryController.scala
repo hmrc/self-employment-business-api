74877c6f09b304351f21181aa8e4f174b463998f
Matt Stephens <97457093+mattstephens-hmrc@users.noreply.github.com>
2022-03-02 15:46:39 +0000

    MTDSA-11143 Added sbt fatal-warnings compiler flag; removed silencer plug-in as it's now integrated into SBT (#81)
    * MTDSA-11143 Added sbt fatal-warnings compiler flag; removed silencer plug-in as it's now integrated into SBT
    
    * MTDSA-11143 Prevent runtime unmatched exceptions in downstream error mapping

--- app/v1/controllers/DeleteSelfEmploymentAnnualSummaryController.scala
+++ app/v1/controllers/DeleteSelfEmploymentAnnualSummaryController.scala
@@ -18,16 +18,16 @@ package v1.controllers
 
 import cats.data.EitherT
 import cats.implicits._
-import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import utils.{IdGenerator, Logging}
+import play.api.mvc.{ Action, AnyContent, ControllerComponents }
+import utils.{ IdGenerator, Logging }
 import v1.controllers.requestParsers.DeleteSelfEmploymentAnnualSummaryRequestParser
 import v1.models.errors._
 import v1.models.request.deleteSEAnnual.DeleteSelfEmploymentAnnualSummaryRawData
-import v1.services.{DeleteSelfEmploymentAnnualSummaryService, EnrolmentsAuthService, MtdIdLookupService}
+import v1.services.{ DeleteSelfEmploymentAnnualSummaryService, EnrolmentsAuthService, MtdIdLookupService }
 
-import scala.concurrent.{ExecutionContext, Future}
+import javax.inject.{ Inject, Singleton }
+import scala.concurrent.{ ExecutionContext, Future }
 
 @Singleton
 class DeleteSelfEmploymentAnnualSummaryController @Inject()(val authService: EnrolmentsAuthService,
@@ -36,7 +36,9 @@ class DeleteSelfEmploymentAnnualSummaryController @Inject()(val authService: Enr
                                                             service: DeleteSelfEmploymentAnnualSummaryService,
                                                             cc: ControllerComponents,
                                                             idGenerator: IdGenerator)(implicit ec: ExecutionContext)
-extends AuthorisedController(cc) with BaseController with Logging {
+    extends AuthorisedController(cc)
+    with BaseController
+    with Logging {
 
   implicit val endpointLogContext: EndpointLogContext =
     EndpointLogContext(controllerName = "DeleteSelfEmploymentAnnualSummaryController", endpointName = "deleteSelfEmploymentAnnualSummary")
@@ -44,12 +46,13 @@ extends AuthorisedController(cc) with BaseController with Logging {
   def handleRequest(nino: String, businessId: String, taxYear: String): Action[AnyContent] =
     authorisedAction(nino).async { implicit request =>
       implicit val correlationId: String = idGenerator.getCorrelationId
-      logger.info(message = s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
-        s"with correlationId : $correlationId")
+      logger.info(
+        message = s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
+          s"with correlationId : $correlationId")
       val rawData = DeleteSelfEmploymentAnnualSummaryRawData(nino, businessId, taxYear)
       val result =
         for {
-          parsedRequest <- EitherT.fromEither[Future](parser.parseRequest(rawData))
+          parsedRequest   <- EitherT.fromEither[Future](parser.parseRequest(rawData))
           serviceResponse <- EitherT(service.deleteSelfEmploymentAnnualSummary(parsedRequest))
         } yield {
           logger.info(
@@ -61,7 +64,7 @@ extends AuthorisedController(cc) with BaseController with Logging {
         }
       result.leftMap { errorWrapper =>
         val resCorrelationId = errorWrapper.correlationId
-        val result = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
+        val result           = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
 
         logger.warn(
           s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
@@ -70,16 +73,13 @@ extends AuthorisedController(cc) with BaseController with Logging {
       }.merge
     }
 
-  private def errorResult(errorWrapper: ErrorWrapper) = {
-    (errorWrapper.error: @unchecked) match {
-      case NinoFormatError |
-           BusinessIdFormatError |
-           BadRequestError |
-           TaxYearFormatError |
-           RuleTaxYearNotSupportedError |
-           RuleTaxYearRangeInvalidError => BadRequest(Json.toJson(errorWrapper))
+  private def errorResult(errorWrapper: ErrorWrapper) =
+    errorWrapper.error match {
+      case NinoFormatError | BusinessIdFormatError | BadRequestError | TaxYearFormatError | RuleTaxYearNotSupportedError |
+          RuleTaxYearRangeInvalidError =>
+        BadRequest(Json.toJson(errorWrapper))
+      case NotFoundError   => NotFound(Json.toJson(errorWrapper))
       case DownstreamError => InternalServerError(Json.toJson(errorWrapper))
-      case NotFoundError => NotFound(Json.toJson(errorWrapper))
+      case _               => unhandledError(errorWrapper)
     }
-  }
 }
