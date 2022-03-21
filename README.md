1042b02db0895fc3f0671f0bc538b619f09ffdc2
Matt Stephens <97457093+mattstephens-hmrc@users.noreply.github.com>
2022-03-21 11:56:23 +0000

    MTDSA-10917 Dependency upgrades (#77)
    * MTDSA-10917 Dependency upgrades
    
    * MTDSA-10917 Updated depdendency upgrades
    
    * MTDSA-10917 Dependency upgrades

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ The Self Employment Business API allows software packages to:
 ## Requirements
 - Scala 2.12.x
 - Java 8
-- sbt 1.3.13
+- sbt 1.6.x
 - [Service Manager](https://github.com/hmrc/service-manager)
 
 ## Development Setup
--- test/v1/routing/VersionRoutingRequestHandlerSpec.scala
+++ test/v1/routing/VersionRoutingRequestHandlerSpec.scala
@@ -23,15 +23,15 @@ import org.scalatest.Inside
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Configuration
 import play.api.http.HeaderNames.ACCEPT
-import play.api.http.{HttpConfiguration, HttpErrorHandler, HttpFilters}
+import play.api.http.{ HttpConfiguration, HttpErrorHandler, HttpFilters }
 import play.api.libs.json.Json
-import play.api.mvc.{EssentialAction, _}
+import play.api.mvc._
 import play.api.routing.Router
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import routing.{VersionRoutingMap, VersionRoutingRequestHandler}
+import routing.{ VersionRoutingMap, VersionRoutingRequestHandler }
 import support.UnitSpec
-import v1.models.errors.{InvalidAcceptHeaderError, UnsupportedVersionError}
+import v1.models.errors.{ InvalidAcceptHeaderError, UnsupportedVersionError }
 
 class VersionRoutingRequestHandlerSpec extends UnitSpec with Inside with MockAppConfig with GuiceOneAppPerSuite {
   test =>
@@ -43,32 +43,32 @@ class VersionRoutingRequestHandlerSpec extends UnitSpec with Inside with MockApp
   import play.api.routing.sird._
 
   object DefaultHandler extends Handler
-  object V1Handler extends Handler
-  object V2Handler extends Handler
-  object V3Handler extends Handler
+  object V1Handler      extends Handler
+  object V2Handler      extends Handler
+  object V3Handler      extends Handler
 
   private val defaultRouter = Router.from {
     case GET(p"") => DefaultHandler
   }
-  private val v1Router      = Router.from {
+  private val v1Router = Router.from {
     case GET(p"/v1") => V1Handler
   }
-  private val v2Router      = Router.from {
+  private val v2Router = Router.from {
     case GET(p"/v2") => V2Handler
   }
-  private val v3Router      = Router.from {
+  private val v3Router = Router.from {
     case GET(p"/v3") => V3Handler
   }
 
   private val routingMap = new VersionRoutingMap {
-    override val defaultRouter: Router = test.defaultRouter
+    override val defaultRouter: Router    = test.defaultRouter
     override val map: Map[String, Router] = Map("1.0" -> v1Router, "2.0" -> v2Router, "3.0" -> v3Router)
   }
 
   class Test(implicit acceptHeader: Option[String]) {
     val httpConfiguration: HttpConfiguration = HttpConfiguration("context")
-    private val errorHandler      = mock[HttpErrorHandler]
-    private val filters           = mock[HttpFilters]
+    private val errorHandler                 = mock[HttpErrorHandler]
+    private val filters                      = mock[HttpFilters]
     (filters.filters _).stubs().returns(Seq.empty)
 
     MockAppConfig.featureSwitch.returns(Some(Configuration(ConfigFactory.parseString("""
@@ -184,7 +184,7 @@ class VersionRoutingRequestHandlerSpec extends UnitSpec with Inside with MockApp
 
         private val request = buildRequest("/v1")
         inside(requestHandler.routeRequest(request)) {
-          case Some(a:EssentialAction) =>
+          case Some(a: EssentialAction) =>
             val result = a.apply(request)
 
             status(result) shouldBe NOT_FOUND
