ec70c65e76109a183e2153fb2557e237f4d7be6c
jeremystone <6453521+jeremystone@users.noreply.github.com>
2022-10-17 14:59:42 +0100

    - add support for TAX_YEAR_NOT_SUPPORTED and other 422 downstream responses

--- app/v1/connectors/httpparsers/StandardDownstreamHttpParser.scala
+++ app/v1/connectors/httpparsers/StandardDownstreamHttpParser.scala
@@ -61,8 +61,8 @@ object StandardDownstreamHttpParser extends HttpParser {
           "[StandardDownstreamHttpParser][read] - " +
             s"Success response received with correlationId: $correlationId when calling $url")
         successOutcomeFactory(correlationId)
-      case BAD_REQUEST | NOT_FOUND | FORBIDDEN | CONFLICT | GONE => Left(ResponseWrapper(correlationId, parseErrors(response)))
-      case _                                                     => Left(ResponseWrapper(correlationId, OutboundError(InternalError)))
+      case BAD_REQUEST | NOT_FOUND | FORBIDDEN | CONFLICT | UNPROCESSABLE_ENTITY | GONE => Left(ResponseWrapper(correlationId, parseErrors(response)))
+      case _ => Left(ResponseWrapper(correlationId, OutboundError(InternalError)))
     }
   }
 
