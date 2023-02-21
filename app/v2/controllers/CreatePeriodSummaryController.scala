a67fd1d019eb6e36ee084e2a895ab5eae5d5b72c
Darshan Balasingam <112945711+darshanbala-hmrc@users.noreply.github.com>
2023-02-21 10:18:59 +0000

    MTDSA-14593 Moved files to v2

---
+++ app/v2/controllers/CreatePeriodSummaryController.scala
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers
+
+import api.controllers.{AuthorisedController, BaseController, EndpointLogContext}
+import api.models.errors._
+import api.hateoas.HateoasFactory
+import api.services.{EnrolmentsAuthService, MtdIdLookupService}
+import cats.data.EitherT
+import cats.implicits._
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, ControllerComponents, Result}
+import utils.{IdGenerator, Logging}
+import v2.controllers.requestParsers.CreatePeriodSummaryRequestParser
+import v2.models.request.createPeriodSummary.CreatePeriodSummaryRawData
+import v2.models.response.createPeriodSummary.CreatePeriodSummaryHateoasData
+import v2.models.response.createPeriodSummary.CreatePeriodSummaryResponse.LinksFactory
+import v2.services.CreatePeriodSummaryService
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class CreatePeriodSummaryController @Inject() (val authService: EnrolmentsAuthService,
+                                               val lookupService: MtdIdLookupService,
+                                               parser: CreatePeriodSummaryRequestParser,
+                                               service: CreatePeriodSummaryService,
+                                               hateoasFactory: HateoasFactory,
+                                               cc: ControllerComponents,
+                                               idGenerator: IdGenerator)(implicit ec: ExecutionContext)
+    extends AuthorisedController(cc)
+    with BaseController
+    with Logging {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(controllerName = "CreateSelfEmploymentPeriodController", endpointName = "createSelfEmploymentPeriodSummary")
+
+  def handleRequest(nino: String, businessId: String): Action[JsValue] =
+    authorisedAction(nino).async(parse.json) { implicit request =>
+      implicit val correlationId: String = idGenerator.getCorrelationId
+      logger.info(
+        message = s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
+          s"with correlationId : $correlationId")
+      val rawData = CreatePeriodSummaryRawData(nino, businessId, request.body)
+      val result =
+        for {
+          parsedRequest   <- EitherT.fromEither[Future](parser.parseRequest(rawData))
+          serviceResponse <- EitherT(service.createPeriodicSummary(parsedRequest))
+        } yield {
+          val hateoasData = CreatePeriodSummaryHateoasData(
+            parsedRequest.nino,
+            parsedRequest.businessId,
+            serviceResponse.responseData.periodId,
+            Some(parsedRequest.taxYear)
+          )
+
+          val vendorResponse = hateoasFactory.wrap(serviceResponse.responseData, hateoasData)
+
+          logger.info(
+            s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+              s"Success response received with CorrelationId: ${serviceResponse.correlationId}")
+
+          Ok(Json.toJson(vendorResponse))
+            .withApiHeaders(serviceResponse.correlationId)
+        }
+
+      result.leftMap { errorWrapper =>
+        val resCorrelationId = errorWrapper.correlationId
+        val result: Result   = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
+
+        logger.warn(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+            s"Error response received with CorrelationId: $resCorrelationId")
+        result
+      }.merge
+    }
+
+  private def errorResult(errorWrapper: ErrorWrapper) =
+    errorWrapper.error match {
+      case MtdErrorWithCode(ValueFormatError.code) | MtdErrorWithCode(RuleIncorrectOrEmptyBodyError.code) =>
+        BadRequest(Json.toJson(errorWrapper))
+
+      case _
+          if errorWrapper.containsAnyOf(
+            BadRequestError,
+            NinoFormatError,
+            ValueFormatError,
+            BusinessIdFormatError,
+            StartDateFormatError,
+            EndDateFormatError,
+            RuleBothExpensesSuppliedError,
+            RuleEndDateBeforeStartDateError,
+            RuleOverlappingPeriod,
+            RuleMisalignedPeriod,
+            RuleNotContiguousPeriod,
+            RuleNotAllowedConsolidatedExpenses,
+            RuleIncorrectOrEmptyBodyError,
+            RuleDuplicateSubmissionError,
+            RuleTaxYearNotSupportedError,
+            RuleInvalidSubmissionPeriodError,
+            RuleInvalidSubmissionEndDateError
+          ) =>
+        BadRequest(Json.toJson(errorWrapper))
+      case NotFoundError => NotFound(Json.toJson(errorWrapper))
+      case InternalError => InternalServerError(Json.toJson(errorWrapper))
+      case _             => unhandledError(errorWrapper)
+    }
+
+}
---
+++ app/v2/controllers/requestParsers/validators/CreatePeriodSummaryValidator.scala
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers.requestParsers.validators
+
+import api.controllers.requestParsers.validators.Validator
+import api.controllers.requestParsers.validators.validations._
+import api.models.errors.{EndDateFormatError, MtdError, StartDateFormatError}
+import v2.controllers.requestParsers.validators.validations.ConsolidatedExpensesValidation
+import v2.models.request.createPeriodSummary._
+
+class CreatePeriodSummaryValidator extends Validator[CreatePeriodSummaryRawData] {
+
+  private val validationSet =
+    List(parameterFormatValidation, bodyFormatValidation, bodyFieldValidation, dateRuleValidation, consolidatedExpensesRuleValidation)
+
+  private def parameterFormatValidation: CreatePeriodSummaryRawData => List[List[MtdError]] = (data: CreatePeriodSummaryRawData) => {
+    List(
+      NinoValidation.validate(data.nino),
+      BusinessIdValidation.validate(data.businessId)
+    )
+  }
+
+  private def bodyFormatValidation: CreatePeriodSummaryRawData => List[List[MtdError]] = { data =>
+    JsonFormatValidation.validateAndCheckNonEmpty[CreatePeriodSummaryBody](data.body) match {
+      case Nil          => NoValidationErrors
+      case schemaErrors => List(schemaErrors)
+    }
+  }
+
+  private def consolidatedExpensesRuleValidation: CreatePeriodSummaryRawData => List[List[MtdError]] = (data: CreatePeriodSummaryRawData) => {
+    val body = data.body.as[CreatePeriodSummaryBody]
+    List(
+      ConsolidatedExpensesValidation.validate(
+        allowableExpenses = body.periodAllowableExpenses,
+        disallowableExpenses = body.periodDisallowableExpenses
+      )
+    )
+  }
+
+  private def bodyFieldValidation: CreatePeriodSummaryRawData => List[List[MtdError]] = { data =>
+    val body = data.body.as[CreatePeriodSummaryBody]
+
+    List(
+      Validator.flattenErrors(
+        List(
+          validateDates(body.periodDates.periodStartDate, body.periodDates.periodEndDate),
+          body.periodIncome.map(validateIncome).getOrElse(NoValidationErrors),
+          body.periodAllowableExpenses.map(validateAllowableExpenses).getOrElse(NoValidationErrors),
+          body.periodDisallowableExpenses.map(validateDisallowableExpenses).getOrElse(NoValidationErrors)
+        )
+      ))
+  }
+
+  private def dateRuleValidation: CreatePeriodSummaryRawData => List[List[MtdError]] = (data: CreatePeriodSummaryRawData) => {
+    val body = data.body.as[CreatePeriodSummaryBody].periodDates
+    List(
+      DateValidation.validateEndDateBeforeStartDate(body.periodStartDate, body.periodEndDate)
+    )
+  }
+
+  private def validateDates(periodStartDate: String, periodEndDate: String): List[MtdError] = {
+    List(
+      DateValidation.validate(
+        field = periodStartDate,
+        error = StartDateFormatError
+      ),
+      DateValidation.validate(
+        field = periodEndDate,
+        error = EndDateFormatError
+      )
+    ).flatten
+  }
+
+  private def validateIncome(periodIncome: PeriodIncome): List[MtdError] = {
+    List(
+      NumberValidation.validateOptional(
+        field = periodIncome.turnover,
+        path = s"/periodIncome/turnover"
+      ),
+      NumberValidation.validateOptional(
+        field = periodIncome.other,
+        path = s"/periodIncome/other"
+      )
+    ).flatten
+  }
+
+  private def validateAllowableExpenses(periodAllowableExpenses: PeriodAllowableExpenses): List[MtdError] =
+    List(
+      NumberValidation.validateOptional(
+        field = periodAllowableExpenses.consolidatedExpenses,
+        path = s"/periodAllowableExpenses/consolidatedExpenses"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.costOfGoodsAllowable,
+        path = s"/periodAllowableExpenses/costOfGoodsAllowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodAllowableExpenses.paymentsToSubcontractorsAllowable,
+        path = s"/periodAllowableExpenses/paymentsToSubcontractorsAllowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodAllowableExpenses.wagesAndStaffCostsAllowable,
+        path = s"/periodAllowableExpenses/wagesAndStaffCostsAllowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodAllowableExpenses.carVanTravelExpensesAllowable,
+        path = s"/periodAllowableExpenses/carVanTravelExpensesAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.premisesRunningCostsAllowable,
+        path = s"/periodAllowableExpenses/premisesRunningCostsAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.maintenanceCostsAllowable,
+        path = s"/periodAllowableExpenses/maintenanceCostsAllowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodAllowableExpenses.adminCostsAllowable,
+        path = s"/periodAllowableExpenses/adminCostsAllowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodAllowableExpenses.businessEntertainmentCostsAllowable,
+        path = s"/periodAllowableExpenses/businessEntertainmentCostsAllowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodAllowableExpenses.advertisingCostsAllowable,
+        path = s"/periodAllowableExpenses/advertisingCostsAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.interestOnBankOtherLoansAllowable,
+        path = s"/periodAllowableExpenses/interestOnBankOtherLoansAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.financeChargesAllowable,
+        path = s"/periodAllowableExpenses/financeChargesAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.irrecoverableDebtsAllowable,
+        path = s"/periodAllowableExpenses/irrecoverableDebtsAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.professionalFeesAllowable,
+        path = s"/periodAllowableExpenses/professionalFeesAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.depreciationAllowable,
+        path = s"/periodAllowableExpenses/depreciationAllowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodAllowableExpenses.otherExpensesAllowable,
+        path = s"/periodAllowableExpenses/otherExpensesAllowable"
+      )
+    ).flatten
+
+  private def validateDisallowableExpenses(periodDisallowableExpenses: PeriodDisallowableExpenses): List[MtdError] = {
+    List(
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodDisallowableExpenses.costOfGoodsDisallowable,
+        path = s"/periodDisallowableExpenses/costOfGoodsDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.paymentsToSubcontractorsDisallowable,
+        path = s"/periodDisallowableExpenses/paymentsToSubcontractorsDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.wagesAndStaffCostsDisallowable,
+        path = s"/periodDisallowableExpenses/wagesAndStaffCostsDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.carVanTravelExpensesDisallowable,
+        path = s"/periodDisallowableExpenses/carVanTravelExpensesDisallowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodDisallowableExpenses.premisesRunningCostsDisallowable,
+        path = s"/periodDisallowableExpenses/premisesRunningCostsDisallowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodDisallowableExpenses.maintenanceCostsDisallowable,
+        path = s"/periodDisallowableExpenses/maintenanceCostsDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.adminCostsDisallowable,
+        path = s"/periodDisallowableExpenses/adminCostsDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.businessEntertainmentCostsDisallowable,
+        path = s"/periodDisallowableExpenses/businessEntertainmentCostsDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.advertisingCostsDisallowable,
+        path = s"/periodDisallowableExpenses/advertisingCostsDisallowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodDisallowableExpenses.interestOnBankOtherLoansDisallowable,
+        path = s"/periodDisallowableExpenses/interestOnBankOtherLoansDisallowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodDisallowableExpenses.financeChargesDisallowable,
+        path = s"/periodDisallowableExpenses/financeChargesDisallowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodDisallowableExpenses.irrecoverableDebtsDisallowable,
+        path = s"/periodDisallowableExpenses/irrecoverableDebtsDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.professionalFeesDisallowable,
+        path = s"/periodDisallowableExpenses/professionalFeesDisallowable"
+      ),
+      NumberValidation.validateOptionalIncludeNegatives(
+        field = periodDisallowableExpenses.depreciationDisallowable,
+        path = s"/periodDisallowableExpenses/depreciationDisallowable"
+      ),
+      NumberValidation.validateOptional(
+        field = periodDisallowableExpenses.otherExpensesDisallowable,
+        path = s"/periodDisallowableExpenses/otherExpensesDisallowable"
+      )
+    ).flatten
+  }
+
+  override def validate(data: CreatePeriodSummaryRawData): List[MtdError] = run(validationSet, data)
+
+}
