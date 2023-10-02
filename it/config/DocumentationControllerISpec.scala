8ca37ad4515242a596977a27c231deb1d9395577
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2023-10-02 09:57:08 +0100

    MTDSA-19495: Added Header test to ISpec

--- it/config/DocumentationControllerISpec.scala
+++ it/config/DocumentationControllerISpec.scala
@@ -18,20 +18,19 @@ package config
 
 import io.swagger.v3.parser.OpenAPIV3Parser
 import play.api.http.Status.OK
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.Json
 import play.api.libs.ws.WSResponse
 import routing.{Version1, Version2, Version3}
 import support.IntegrationBaseSpec
-import uk.gov.hmrc.auth.core.ConfidenceLevel
 
 import scala.util.Try
 
 class DocumentationControllerISpec extends IntegrationBaseSpec {
 
-  val config: AppConfig                = app.injector.instanceOf[AppConfig]
-  val confidenceLevel: ConfidenceLevel = config.confidenceLevelConfig.confidenceLevel
+  private val config          = app.injector.instanceOf[AppConfig]
+  private val confidenceLevel = config.confidenceLevelConfig.confidenceLevel
 
-  val apiDefinitionJson: JsValue = Json.parse(
+  private val apiDefinitionJson = Json.parse(
     s"""
     |{
     |   "scopes":[
@@ -100,6 +99,19 @@ class DocumentationControllerISpec extends IntegrationBaseSpec {
           openAPI.getInfo.getTitle shouldBe "Self Employment Business (MTD)"
         }
         openAPI.getInfo.getVersion shouldBe version.toString
+      }
+
+      s"return the documentation with the correct accept header for version $version" in {
+        val response = get(s"/api/conf/${version.name}/common/headers.yaml")
+        val body     = response.body[String]
+
+        val headerRegex = """(?s).*?application/vnd\.hmrc\.(\d+\.\d+)\+json.*?""".r
+        val header      = headerRegex.findFirstMatchIn(body)
+        header.isDefined shouldBe true
+
+        val versionFromHeader = header.get.group(1)
+        versionFromHeader shouldBe version.name
+
       }
     }
   }
